29e00d55e839ba0c2d7480b7265c815d1bacc53b
ImShubham09 <singhashubham612@gmail.com>
2024-12-03 14:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
-const leapyear	(year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0))
-	
-	return leapyear?366:365;
+ const leapyear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  
+  // Return 366 for leap years, else 365
+  return leapyear ? 366 : 365;
 }
 console.log(daysOfAYear(2022));
 console.log(daysOfAYear(2024));
